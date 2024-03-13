a479e7c38eab053fa955c67453474edad555e5bc
Pedro Nicolas Costa <pedroniickolas@gmail.com>
2024-03-13 12:35:08 -0300

    navbar responsivo

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,44 +1,51 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
+  constructor(mobileMenu) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.sidebar = document.querySelector('.sidebar');
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.sidebar.style.display = this.mobileMenu.classList.contains(this.activeClass) ? 'flex' : 'none'; // Toggle display based on activeClass
+    this.animateLinks();
+  }
+
+  handleOutsideClick(event) {
+    if (!this.mobileMenu.contains(event.target) && !this.sidebar.contains(event.target) && this.mobileMenu.classList.contains(this.activeClass)) {
+      this.mobileMenu.classList.remove(this.activeClass);
+      this.sidebar.style.display = 'none';
       this.animateLinks();
     }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
   }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+    document.addEventListener("click", this.handleOutsideClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+);
+mobileNavbar.init();
